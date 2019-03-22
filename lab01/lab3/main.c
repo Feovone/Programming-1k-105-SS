8baeb2f00d3680aa45b29f48024aa117ccd0fcc5
Feovone <cawacawa17@gmail.com>
2019-03-22 18:49:49 +0200

    lab3.1

--- lab01/lab3/main.c
+++ lab01/lab3/main.c
@@ -3,18 +3,46 @@
 #include <math.h>
 int main()
 {
-   int res;
-   int a[5]={8,2,3,4,5,7};
-   int sz=sizeof(a)/sizeof(a[0]);
-  for(int i=0;i<sz;i++){
-    int k=a[i];
-    int j=i+1;
-    for(;j>=0 && a[j]>k;){
-  a[j]=a[j-1];
+   int q,sz,z=-1;
+   int e=0;
 
-  }}
-  for (int p=0;p<=sz;p++){
+   printf("Please enter size of massive\n");
+   scanf("%d",&sz);
+   int a[sz];
+printf("Enter array numbers\n");
+   while(e!=-1){
+
+   scanf("%d",&q);
+   a[e]=q;
+   e++;
+   z++;
+   if(q==-1){
+    e=-1;
+   }
+      }
+  if (z>sz || z<sz){
+    printf("ERROR:INVALID SIZE OF MASSIVE");
+}
+//int sz=sizeof(a)/sizeof(a[0]);
+
+
+  for(int i=1;i<sz;i++){
+    int k=a[i];
+    int j=i-1;
+
+    while(j>=0 && a[j]>k){
+     a[j+1]=a[j];
+     j=j-1;
+     for (int p=0;p<sz;p++){
+   // printf("\n--%d",a[p]);
+  }
+    }
+     a[j+1]=k;
+   //printf("\n %d___%d\n",a[i],a[j]);
+  }
+for (int p=0;p<sz;p++){
     printf("%d",a[p]);
   }
        return 0;
+
 }
