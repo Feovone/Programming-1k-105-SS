203c7804741df91325344dcd74d6244d8767a45e
Shevchenko Sasha <47010241+Feovone@users.noreply.github.com>
2019-05-10 14:16:17 +0300

    Add files via upload

--- lab6/main.c
+++ lab6/main.c
@@ -2,30 +2,53 @@
 #include <stdlib.h>
 #include <math.h>
 #include <ctype.h>
+#include <string.h>
 #define ID 10
 #define NAME 30
 #define TEL 14
 #define KURS 3
 #define LEN 50
 
-int main()
-{
-    struct student{
-        int id[ID];
+
+struct student{
+        int id;
         char name[NAME];
-        int tel[TEL];
-        int kurs[KURS];
+        char tel[TEL];
+        char kurs[KURS];
         int math;
         int phych;
-    }s;
-    FILE *ishodnik;
-    FILE *resual;
-    int j=
-    for(int j=0;j<=ID;j++){
-    for(int i=0;i<=ID;i++){
-    while(s(j).id[]!=','){
-    fread(s(j).id,1,1,ishodnik);
+};
+
+struct student* fscan(FILE* fp, struct student* s) {
+    if (!fp) {
+        return NULL;
     }
+    char buffer[256] = {0};
+    fscanf(fp, "%[^\n]s", buffer);
+    sscanf(buffer, "%d,%s,%s,%s,math:%d,phych:%d",
+               &(s->id), s->name, s->tel, s->kurs,&(s->math), &(s->phych));
+   // fscanf(fp, "%d,%s,%s,%s,math:%d,phych:%d;\n",
+   //            &(s->id), s->name, s->tel, s->kurs,&(s->math), &(s->phych));
+    return s;
+};
+
+void print(const struct student* s) {
+    printf("%d,%s,%s,%s,math:%d,phych:%d\n",
+            s->id, s->name, s->tel, s->kurs,s->math, s->phych);
+}
+
+int main()
+{
+    struct student s;
+    memset(&s, 0, sizeof(s));
+    char fname[256] = "stud.txt";
+    FILE *fp = fopen(fname, "r");
+    if (!fp) {
+        printf("Can't open file %s\n", fname);
+        return -1;
     }
 
+    fscan(fp, &s);
+    print(&s);
+    fclose(fp);
 }
